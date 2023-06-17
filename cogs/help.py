d3b697ea46e70534cbe7cbf38a1802e9b2d04545
Zerbaib <hugo.hennetin@proton.me>
2023-06-17 02:57:52 +0200

    [REDO] 1

--- cogs/help.py
+++ cogs/help.py
@@ -1,59 +1,51 @@
 import disnake
 from disnake.ext import commands
+import json
 
 class HelpCog(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot, config):
         self.bot = bot
+        self.config = config
 
     @commands.slash_command(
         name='help',
-        description='Displays the list of available commands by category',
+        description='Displays the list of available commands by cog',
         options=[
             disnake.Option(
-                name='category',
-                description='The category of commands to display',
                 type=3,
-                required=False
+                name='cog',
+                description='The cog to display commands for',
+                required=True,
+                choices=[
+                    disnake.OptionChoice(name=cog, value=cog) for cog in self.config['extensions'] if cog != 'help'
+                ]
             )
         ]
     )
-    async def help_command(self, inter, category: str = None):
-        if category is None:
-            # Display the list of categories
-            categories = [cog.qualified_name for cog in self.bot.cogs.values()]
-            category_list = '\n'.join(f'`{category}`' for category in categories)
-            embed = disnake.Embed(
-                title='List of Categories',
-                description=category_list,
-                color=disnake.Color.blurple()
-            )
-            await inter.response.send_message(embed=embed)
-        else:
-            # Display the commands of a specific category
-            category_cog = self.bot.get_cog(category)
-            if category_cog is None:
-                embed = disnake.Embed(
-                    title='Error',
-                    description=f'The category "{category}" does not exist',
-                    color=disnake.Color.red()
-                )
-                await inter.response.send_message(embed=embed)
-            else:
-                command_list = []
-                for command in category_cog.get_commands():
-                    command_info = f'`{command.name}`: {command.description}\n'
-                    command_list.append(command_info)
-                commands_info = '\n'.join(command_list)
-                embed = disnake.Embed(
-                    title=f'Commands in category "{category}"',
-                    description=commands_info,
-                    color=disnake.Color.blurple()
-                )
-                await inter.response.send_message(embed=embed, ephemeral=True)
+    async def help_command(self, inter, cog: str):
+        cog_name = cog.capitalize() + 'Cog'
+        if cog_name not in self.bot.cogs:
+            await inter.response.send_message('This cog does not exist.')
+            return
+
+        cog_obj = self.bot.get_cog(cog_name)
+        commands = cog_obj.get_commands()
+
+        if not commands:
+            await inter.response.send_message('This cog doesn\'t have commands.')
+            return
+
+        embed = disnake.Embed(title=f'Commands in {cog_name}', color=disnake.Color.blurple())
+        for command in commands:
+            embed.add_field(name=command.name, value=command.description, inline=False)
+
+        await inter.response.send_message(embed=embed)
 
     @commands.Cog.listener()
     async def on_ready(self):
         print('Help cog is ready!')
 
 def setup(bot):
-    bot.add_cog(HelpCog(bot))
+    with open('config.json', 'r') as config_file:
+        config = json.load(config_file)
+    bot.add_cog(HelpCog(bot, config))
