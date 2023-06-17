d846bd6d5ff6c2766fb3069dc0b0dc7f4ed6e855
Zerbaib <hugo.hennetin@proton.me>
2023-06-17 03:47:02 +0200

    Revert "[REDO] for better lisibility in config"
    This reverts commit a38500f240a5895886d66793bc30f9b4d9b0a053.

--- cogs/status.py
+++ cogs/status.py
@@ -14,17 +14,17 @@ class StatusCog(commands.Cog):
     async def update_status(self):
         await self.bot.wait_until_ready()
         while not self.bot.is_closed():
-            guild = self.bot.get_guild(self.config['server_id'])
+            guild = self.bot.get_guild(self.config.get('server_id'))
             if guild:
                 member_count = len(guild.members)
             else:
                 member_count = 0
 
-            status_message = random.choice(self.config['status']['messages'])
+            status_message = random.choice(self.config.get('status_messages'))
             status_message = status_message.replace('{member_count}', str(member_count))
-            status_message = status_message.replace('{bot_version}', self.config['bot_version'])
+            status_message = status_message.replace('{bot_version}', self.config.get('bot_version', ''))
 
-            status_type = self.config['status']['type']
+            status_type = self.config.get('status_type')
             if status_type == 'watching':
                 await self.bot.change_presence(activity=disnake.Activity(type=disnake.ActivityType.watching, name=status_message))
             elif status_type == 'playing':
@@ -32,7 +32,7 @@ class StatusCog(commands.Cog):
             elif status_type == 'custom':
                 await self.bot.change_presence(activity=disnake.Activity(type=disnake.ActivityType.custom, name=status_message))
 
-            await asyncio.sleep(self.config['status']['interval'])
+            await asyncio.sleep(self.config.get('status_interval'))
 
     @commands.Cog.listener()
     async def on_ready(self):
